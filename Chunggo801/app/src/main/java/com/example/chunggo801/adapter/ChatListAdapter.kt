a18ed9753f83c7aeb0e593db95a8cfc9368fb3d3
eunyoung kim <snu9451@gmail.com>
2021-07-15 20:31:21 +0900

    add

--- Chunggo801/app/src/main/java/com/example/chunggo801/adapter/ChatListAdapter.kt
+++ Chunggo801/app/src/main/java/com/example/chunggo801/adapter/ChatListAdapter.kt
@@ -17,7 +17,7 @@ class ChatListAdapter: ListAdapter<ChatList, ChatListAdapter.ChatListViewHolder>
             binding.chatNickname.text = chatListModel.chat_nickname
             binding.chatContent.text = chatListModel.chat_content
             /*
-            // 서버로부터 이미지 정보를 가져와서 이미지 로딩을 처리하기
+            // 서버로부터 이미지 정보를 가져와서 이미지 로딩을 처리하기f
             Glide
                 .with(binding.coverImageView.context)  // binding이 가리키는 것은 xml 즉, ViewGroup
                 .load(bookModel.coverSmallUrl)
